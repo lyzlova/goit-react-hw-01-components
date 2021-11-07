c939a583fb58432515c2668354c2f7206f0ad75b
Ivanna Lyzlova <49069055+lyzlova@users.noreply.github.com>
2021-11-08 02:51:03 +0300

    Revert "added config"
    This reverts commit 167c66a3c805d1ae3570dbdaaf94c335d8d6202d.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import statisticalData from './data/statistical-data.json';
 import friends from './data/friends.json';
 import Profile from "./components/Profile/Profile";
 import Statistics from "./components/Statistics/Statistics";
-// import FriendList from "./components/FriendList/FriendList";
+import FriendList from "./components/FriendList/FriendList";
 
 function App() {
   return (
